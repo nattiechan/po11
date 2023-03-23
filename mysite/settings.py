382f3c14e2317ade66f8936aa927704f99860c4d
Nattie C <47332368+nattiechan@users.noreply.github.com>
2023-03-22 21:04:53 -0700

    Setup backend and admin site
    - Installed and configured pylint
    - Initalized DB (SQLite)
    - Create Question and Choice models in `polls`
    - Setup admin access for `Question` model

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -105,7 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Los_Angeles'
+
+USE_L10N = True
 
 USE_I18N = True
 
--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,23 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db.models import Model, CharField, DateTimeField, ForeignKey, CASCADE, IntegerField
+from django.utils import timezone
+
+
+class Question(Model):
+    question_text: CharField = CharField(max_length=200)
+    pub_date: DateTimeField = DateTimeField('date published')
+
+    def __str__(self) -> str:
+        return self.question_text
+    
+    def was_published_recently(self) -> bool:
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+class Choice(Model):
+    question: ForeignKey = ForeignKey(Question, on_delete=CASCADE)
+    choice_text: CharField = CharField(max_length=200)
+    votes: IntegerField = IntegerField(default=0)
+
+    def __str__(self) -> str:
+        return self.choice_text
